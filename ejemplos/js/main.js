566885ec5b53c5a75caa5708aa00d64ac9623805
Greg Sánchez <gso@nextret.net>
2019-05-29 11:07:24 +0200

    primer modelo tensorflowjs

---
+++ ejemplos/js/main.js
@@ -0,0 +1,19 @@
+
+async function learnLinear () {
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units: 1, inputShape:[1]}));
+
+    model.compile({
+        loss : 'meanSquaredError',
+        optimizer: 'sgd'
+    });
+
+    const xs = tf.tensor2d([-1,0,1,2,3,4],[6,1]);
+    const ys = tf.tensor2d([-3,-1,1,3,5,7],[6,1]);
+
+    await model.fit(xs,ys, {epochs:500});
+    document.getElementById('output').innerHTML = model.predict(tf.tensor2d([20],[1,1]));
+}
+
+
+document.addEventListener('DOMContentLoaded', learnLinear);
